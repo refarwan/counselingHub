c7e671aaba2f5853d99ef7e2a7f23b8f68fc45cb
Aan Refarwan <refarwan@gmail.com>
2024-11-12 15:08:13 +0700

    feat: add endpoint POST account/register

--- konseling-hub-api/src/modules/account/account.controller.ts
+++ konseling-hub-api/src/modules/account/account.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AccountService } from './account.service';
 
 @Controller('account')
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
+
+  @Post('register')
+  async register(): Promise<{ message: string }> {
+    return { message: 'Register Account' };
+  }
 }
