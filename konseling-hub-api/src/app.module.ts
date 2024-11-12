587b77237338606d021e226d2d30f230b9c1bfb7
Aan Refarwan <refarwan@gmail.com>
2024-11-12 14:59:37 +0700

    feat: create auth module

--- konseling-hub-api/src/app.module.ts
+++ konseling-hub-api/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 
 import { ConfigModule } from '@nestjs/config';
 import { AccountModule } from './modules/account/account.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { AccountModule } from './modules/account/account.module';
       isGlobal: true,
     }),
     AccountModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
