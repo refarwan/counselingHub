b75042fb40d293b10f3d788642bba72d29078c83
Aan Refarwan <refarwan@gmail.com>
2024-11-30 13:51:55 +0700

    feat: add server action configuration

--- konseling-hub-app/next.config.ts
+++ konseling-hub-app/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	experimental: {
+		serverActions: {
+			allowedOrigins: [process.env.APP_ORIGIN as string],
+		},
+	},
 	async rewrites() {
 		return [
 			{
