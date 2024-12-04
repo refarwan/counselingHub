a0a1c5830d553a7ff5aab54ee5ba0b08d7782600
Aan Refarwan <refarwan@gmail.com>
2024-12-04 23:26:49 +0700

    refactor: migtare ESLINT config file

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'API is active';
-  }
+	getHello(): string {
+		return "API is active";
+	}
 }
