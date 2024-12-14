1ae337bfde7201530f3ae245683846a8a5663449
Aan Refarwan <refarwan@gmail.com>
2024-12-14 23:00:26 +0700

    refactor: take it out backend project and remove frontend project

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
 FROM node:20.18-alpine
 
-ARG API_ORIGIN
-ARG IS_HTTPS
-
 WORKDIR /app
 
 COPY . .
 
+RUN apk add --no-cache openssl
 RUN npm install
 RUN npm run build
 
--- app/src/app/anggota/daftar-konselor/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import UnderDevelopment from "../components/UnderDevelopment";
-
-const Page = () => {
-	return <UnderDevelopment />;
-};
-
-export default Page;
